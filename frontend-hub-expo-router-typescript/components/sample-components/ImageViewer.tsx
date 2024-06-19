82a89fe6d9fbe597e07af0b2c1dc5afbb579c17a
Destan <destantezel@googlemail.com>
2024-06-19 15:39:00 +0100

    Create tutorial ImageViewer component and add type safety for TS

---
+++ frontend-hub-expo-router-typescript/components/sample-components/ImageViewer.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Image } from 'react-native';
+
+// Change type definition to match your props
+type ImageViewerProps = {
+  placeholderImageSource: any;
+};
+
+export default function ImageViewer({ placeholderImageSource } : ImageViewerProps) {
+  return (
+    <Image source={placeholderImageSource} style={styles.image} />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
