3e4fc59ecf8861508372a9156777a0743664c614
Destan <destantezel@googlemail.com>
2024-06-19 15:40:41 +0100

    Delete unused import

--- frontend-hub-expo-router-typescript/components/sample-components/Button.tsx
+++ frontend-hub-expo-router-typescript/components/sample-components/Button.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type ButtonProps = {
