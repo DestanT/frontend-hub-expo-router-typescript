863656be2d1148e5773e57260cdbf266bbb2e23b
Destan <destantezel@googlemail.com>
2024-06-20 15:15:27 +0100

    Add selectedImage property and conditional statement: selectedImage > placeholderImage

--- frontend-hub-expo-router-typescript/components/sample-components/ImageViewer.tsx
+++ frontend-hub-expo-router-typescript/components/sample-components/ImageViewer.tsx
@@ -3,10 +3,13 @@ import { StyleSheet, Image } from 'react-native';
 // Change type definition to match your props
 type ImageViewerProps = {
   placeholderImageSource: any;
+  selectedImage: string | null;
 };
 
-export default function ImageViewer({ placeholderImageSource }: ImageViewerProps) {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+export default function ImageViewer({ placeholderImageSource, selectedImage }: ImageViewerProps) {
+  const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
